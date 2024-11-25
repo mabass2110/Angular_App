ae11f5032c9b4798972a0ff4f36b5b3716f34731
Helder Mabasso <hfmabasso@gmail.com>
2024-11-25 15:40:17 +0200

    Setting up a search form and importing external style files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styleUrls: ['home.component.css'],
 })
 export class HomeComponent {
 
 }
+
