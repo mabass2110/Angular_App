bdb8e0533bee961b1b35dee682d7d106aa776cd0
Helder Mabasso <hfmabasso@gmail.com>
2024-11-25 16:26:04 +0200

    Creating housing locations interface, and using it to set up mocked data.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { Housinglocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, HousingLocationComponent],
   template: `
     <section>
 
@@ -24,5 +24,18 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 })
 export class HomeComponent {
 
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+  
 }
 
